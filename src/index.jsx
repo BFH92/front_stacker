d9064d251c9182cd8fa764e4db663782e5cc746c
Valentin Bousquet <bousquet.valentin@gmail.com>
2021-09-13 15:17:13 +0200

    Forgot / in URL

--- src/index.jsx
+++ src/index.jsx
@@ -26,7 +26,7 @@ const App = () => {
             <Route path="/" exact render={() => <Home />} />
             <Route path="/about" render={() => <About />} />
             <Route path="/stacks" render={() => <Stacks />} />
-            <Route path="/searchcompagny" render={() => <SearchCompagny />} />
+            <Route path="/search/compagny" render={() => <SearchCompagny />} />
             <Route path="/compagny" render={() => <Compagny key={uuidv4()} />} />
             <Route path="/user/dashboard" render={() => <UserDashboard key={uuidv4()} />} />
             <Route path="/compagny/dashboard" render={() => <CompagnyDashboard key={uuidv4()} />} />
