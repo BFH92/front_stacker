99c93c4b88475bb986389b2cbbdbe1ce3258fac5
VeraTuran <vmeturan@gmail.com>
2021-09-14 22:01:19 +0200

    user dashboard correct layout, create component EditUserPresentation

--- src/Components/UserPresentation/index.jsx
+++ src/Components/UserPresentation/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router';
+import { useParams } from 'react-router-dom';
 
 const UserPresentation = () => {
     return (
@@ -7,7 +7,9 @@ const UserPresentation = () => {
             <h2>Presentation</h2>
             <div className="presentation--all">
                 <div className="all--items">
-                    dnlekznclkznc
+                    <ul>
+                        {/*<li>{user.first_name}</li>*/}
+                    </ul>
                 </div>
             </div>
         </div>
