d471a00f1857eb8da9a83d820e762b491b24fad0
VeraTuran <vmeturan@gmail.com>
2021-09-14 19:27:48 +0200

    static userPresentation component

---
+++ src/Components/UserPresentation/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useParams } from 'react-router';
+
+const UserPresentation = () => {
+    return (
+        <div className="presentation__main--grid">
+            <h2>Presentation</h2>
+            <div className="presentation--all">
+                <div className="all--items">
+                    dnlekznclkznc
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default UserPresentation
