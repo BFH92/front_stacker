ebad0d79d82dab643477e43e63edcc3fe86ff72c
shanks <cheikh.kebe.01@gmail.com>
2021-09-14 15:19:09 +0200

    styling forms

--- src/Pages/UserSignIn/index.jsx
+++ src/Pages/UserSignIn/index.jsx
@@ -1,39 +1,37 @@
 import React, { useState, useEffect } from "react";
-import SignInForm from '../../Components/Forms/SignInForm';
+import SignInForm from "../../Components/Forms/SignInForm";
 import UsersAPIManager from "../../Services/RailsApi/UsersFetch";
-import { useHistory } from 'react-router';
-import { useDispatch, useSelector} from 'react-redux';
-import {RegisterUserLoginStatus, RegisterUserLogoutStatus} from '../../Store'
+import { useHistory } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { RegisterUserLoginStatus, RegisterUserLogoutStatus } from "../../Store";
 import { Link } from "react-router-dom";
 
-  
+const UserSignIn = ({ user }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-const UserSignIn = ({user}) => {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const history = useHistory()
-  const dispatch = useDispatch()
   const login = async (e) => {
     e.preventDefault();
     const response = await UsersAPIManager.login(email, password);
-    history.push("/")
-    response.status === 200? dispatch(RegisterUserLoginStatus(response.data.user_id,"user")):dispatch(RegisterUserLogoutStatus());
-    user.setIsLogged(true)
+    history.push("/");
+    response.status === 200
+      ? dispatch(RegisterUserLoginStatus(response.data.user_id, "user"))
+      : dispatch(RegisterUserLogoutStatus());
+    user.setIsLogged(true);
   };
-  
+
   return (
     <>
-    <div>
-      <SignInForm user={{email, setEmail, password, setPassword, login}}/>
-    </div>
-    <Link to="/users/sign-up">
-    <div>
-    Pas de compte ? S'inscrire
-    </div>
-    </Link>
-
+      <div>
+        <SignInForm user={{ email, setEmail, password, setPassword, login }} />
+      </div>
+      <Link to="/users/sign-up">
+        <p>Pas de compte ? S'inscrire</p>
+      </Link>
     </>
   );
-}
+};
 
 export default UserSignIn;
