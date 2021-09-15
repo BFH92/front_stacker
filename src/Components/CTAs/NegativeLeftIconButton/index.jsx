e599d4ef6cb55eb4f84ff9abd2850e20a3bfb15a
Valentin Bousquet <bousquet.valentin@gmail.com>
2021-09-15 16:16:22 +0200

    Adding 2 Negative Buttons

---
+++ src/Components/CTAs/NegativeLeftIconButton/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
+const NegativeCustomizedButton = withStyles({
+  root: {
+    borderRadius: 50,
+    boxShadow: 'none',
+    textTransform: 'none',
+    padding: '6px 12px',
+    border: '2px solid rgb(255, 255, 255)',
+    backgroundColor: 'rgb(92, 30, 226)',
+    color: 'rgb(255, 255, 255)',
+    '&:hover': {
+      boxShadow: 'none',
+    },
+    '&:active': {
+      boxShadow: 'none',
+    },
+    '&:focus': {
+      boxShadow: 'none',
+    },
+  },
+})(Button);
+
+const NegativeLeftIconButton = (props) => {
+
+  return (
+    <div>
+      <NegativeCustomizedButton
+        endIcon={props.svg}
+      >
+        {props.name}
+      </NegativeCustomizedButton>
+    </div>
+  );
+};
+
+export default NegativeLeftIconButton;
+
--- src/Components/FilterSystem/index.jsx
+++ src/Components/FilterSystem/index.jsx
@@ -3,15 +3,24 @@ import './filterSystem.scss';
 import ChipsArray from './ChipsArray';
 import RadioButtonsGroup from './RadioGroup';
 // import SimpleSlider from './SimpleSlider';
+import NegativeLeftIconButton from '../CTAs/NegativeLeftIconButton';
+import Save from '../../Assets/Svg/UI/Save';
 
 const FilterSystem = () => {
 
   return (
-    <div className="container_filter--system">
-      <div className="grid__filter--groups">
-        <ChipsArray />
-        <RadioButtonsGroup />
-        {/* <SimpleSlider /> */}
+    <div className="container__filter--system">
+      <div className="container--top">
+        <div className="grid__filter--groups">
+          <ChipsArray />
+          <RadioButtonsGroup />
+          {/* <SimpleSlider /> */}
+        </div>
+      </div>
+      <div className="container--bottom">
+        <div className="container__cta">
+          <NegativeLeftIconButton name={"Enregistrer"} svg={<Save />}/>
+        </div>
       </div>
     </div>    
   );
