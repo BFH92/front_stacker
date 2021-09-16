4bbc3296c9d6f33b065ff594576b6cf05ef8abd7
Valentin Bousquet <bousquet.valentin@gmail.com>
2021-09-16 13:52:57 +0200

    CTA RecentSearch

---
+++ src/Assets/Svg/UI/LeftSimpleArrow/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const LeftSimpleArrow = () => {
+
+  return (
+    <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+        <title>Iconly/Light/Arrow - Left 2</title>
+        <g id="Iconly/Light/Arrow---Left-2" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round">
+            <g id="Arrow---Left-2" transform="translate(12.000000, 12.000000) rotate(-270.000000) translate(-12.000000, -12.000000) translate(5.000000, 8.500000)" stroke="#000000" strokeWidth="1.5">
+                <polyline id="Stroke-1" points="14 0 7 7 0 0"></polyline>
+            </g>
+        </g>
+    </svg>
+  );
+};
+
+export default LeftSimpleArrow;
--- src/Components/RecentSearch/PreviewRecentSearch/index.jsx
+++ src/Components/RecentSearch/PreviewRecentSearch/index.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import './previewRecentSearch.scss';
 import ChipsArray from '../../ResultsCompanies/CompanyPreview/ChipsArray';
+import LeftSimpleArrow from '../../../Assets/Svg/UI/LeftSimpleArrow'
 
 const PreviewRecentSearch = () => {
 
   return (
     <li className="recent--search--item">
-      <h3>23 Résultats</h3>
-      <ChipsArray />
-
-      <div className="container__date">
-        <p>DATE: 13/11/2021</p>
-        <p>18:12</p>
+      <div className="container__cta">
+        <LeftSimpleArrow />
+      </div>
+      <div className="container__informations">
+        <h3>23 Résultats</h3>
+        <ChipsArray />
+        <div className="container__date">
+          <p>DATE: 13/11/2021</p>
+          <p>18:12</p>
+        </div>
       </div>
     </li>
   );
