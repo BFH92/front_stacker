f1dbddf3bde8057712bc4f81771e86d7c13a1179
Valentin Bousquet <bousquet.valentin@gmail.com>
2021-09-21 15:22:23 +0200

    update Cardheader

--- src/Components/CompaniesResults/CompanyPreview/index.jsx
+++ src/Components/CompaniesResults/CompanyPreview/index.jsx
@@ -1,21 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import AddToFavorite from "../../CTAs/AddToFavorite";
 import ChipsArray from "./ChipsArray";
 import CompanyPreviewAvatar from "./Avatar";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import { useSelector } from "react-redux";
 
 const CompanyPreview = ({ company }) => {
-  const isLogged = useSelector(state => state.user.isLogged)
+  const isLogged = useSelector(state => state.user.isLogged);
+
   return (
     <Card variant="outlined">
-      <CardHeader sx={{ p: 2 }}
+      {isLogged ? (
+        <CardHeader sx={{ p: 2 }}
         avatar={
           <CompanyPreviewAvatar companyName={company.name}/>
         }
@@ -24,7 +23,16 @@ const CompanyPreview = ({ company }) => {
         }
         title={company.name}
         subheader={company.company_category_id}
-      />
+        />          
+      ) : (
+        <CardHeader sx={{ p: 2 }}
+        avatar={
+          <CompanyPreviewAvatar companyName={company.name}/>
+        }
+        title={company.name}
+        subheader={company.company_category_id}
+        />
+      )}
       <Divider light/>
       <Card sx={{ p: 2 }}>
         <Typography variant="body2" sx={{ pb: 2 }} color="text.secondary">
