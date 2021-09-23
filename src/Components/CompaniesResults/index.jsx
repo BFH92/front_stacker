11bd1482fc561f31003020833c6a93b963f9c8ec
Benoit Figea <60694673+BFH92@users.noreply.github.com>
2021-09-24 00:30:36 +0200

    V1 fetch by shortList

--- src/Components/CompaniesResults/index.jsx
+++ src/Components/CompaniesResults/index.jsx
@@ -13,41 +13,48 @@ const ResultsCompanies = () => {
 
   const [companiesList, setCompaniesList] = useState([])
   const[shortListIndex, setShortListIndex] = useState(0)
-  let list = new Set()
-  const getShortList = async(shorListIndex) => {  
+
+  const getShortList = async(shorListIndex,idsList, itemList) => {  
     const response = await CompanyInfoManager.getAllCompanies(url, shorListIndex)
     if (response.data){
       let data = response.data
-      
-      data.map((companyData)=>
-        list.add(companyData)
+
+      data.map((companyData)=>{
+        idsList.add(companyData.id)
+        itemList.push(companyData)
+      }
       )
     }
-    let array_list = Array.from(list)
-    console.log(response)
-    console.log(array_list)
-    setCompaniesList(array_list)
-    
-
+    idsList = Array.from(idsList)
+    let array_list = []
+    idsList.map((id)=>
+    array_list.push(itemList.find(element => element.id == id))
+    )
+    setCompaniesList(array_list) 
+  
   }
   const getNextList = (ListIndex) => {
-    companiesList.map((company)=>
-      list.add(company)
+    let idsList = new Set()
+    let itemList = []
+    companiesList.map((company)=>{
+      idsList.add(company.id)
+      itemList.push(company)
+    }
     )
-    console.log(ListIndex)
-  
-    getShortList(ListIndex+1)
-    setShortListIndex(ListIndex+1)
+    ListIndex = ListIndex+1
+    getShortList(ListIndex,idsList,itemList)
+    setShortListIndex(ListIndex)
   
     
   }
 
   useEffect(()=>{
-    list = new Set()
+    let list = new Set()
+    let itemList = []
     setShortListIndex(0)
-    console.log("hey")
+    console.log(list )
     setCompaniesList([])
-    getShortList(0)
+    getShortList(0, list, itemList)
     
   }, [url]);
 
