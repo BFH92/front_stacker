7a978ab2dc99a67d9eedfd21119a7f1fb18b66e5
VeraTuran <vmeturan@gmail.com>
2021-09-24 16:34:16 +0200

    wireframe aboutpage

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -1,12 +1,89 @@
-import React from 'react';
-import '../../Styles/main.scss'
+import React, { useEffect, useRef } from "react";
+import lottie from 'lottie-web';
+//styles
+import "./about.scss";
+import Aos from 'aos';
+import "aos/dist/aos.css";
+//materialUI
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+//Typewriter
+import Typewriter from "typewriter-effect";
+import { Paper } from "@material-ui/core";
+import UIButton from "../../Components/UIButton";
+
 
 const About = () => {
+  const container = useRef(null);
+
+  useEffect(() => {
+    lottie.loadAnimation({
+     container: container.current,
+     renderer: "svg",
+     loop: true,
+      autoplay: true,
+      animationData: require("../../Assets/Svg/Animation/stacker2.json"),
+    });
+  }, []);
+
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
 
   return (
-    <div className="">
-      <div className="container__title-pane">About</div>
+  <div className="container__home">
+    <div className="container__home--left">
+      <div className="content--text">
+        <Typography variant="h1" component="h1"> 
+          Stacker c'est
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter.typeString("répérer")
+                .pauseFor(2500)
+                .start()
+                .pauseFor(200)
+                .deleteChars(7)
+                .typeString("découvrir")
+                .pauseFor(2500)
+                .start()
+                .pauseFor(200)
+                .deleteChars(9)
+                .typeString("trouver");
+            }}
+          />
+        </Typography>
+        <Typography variant="body1" sx={{ mt: 5, mb: 5 }}>
+          Stacker.io offre de naviguer des entreprises par leur Stack pour les devlopeurs freelance ou en recherche d'emploi. <br />
+          En tant que devs en fin de formation, nous voulions rendre plus souple et transparent le processus pour chacun de trouver sa niche selon son expertise.
+        </Typography>
+      </div>
     </div>
+    <div className="container__home--right">
+      <Paper elevation={10} className="mask--color"></Paper>
+      <div className="content--svg" ref={container}></div>
+    </div>
+
+    <div className="container__home--left">
+      <div className="content--text">
+      <Typography variant="h2" component="h2">Pour Devs</Typography>
+        <Typography variant="body1" sx={{ mt: 5, mb: 5 }}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+          blanditiis tenetur unde suscipit, quam beatae rerum inventore
+          consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+          fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
+      </div>
+    </div>
+
+    <div className="container__home--left">
+      <div className="content--text">
+      <Typography variant="h2" component="h2">Pour Entreprise</Typography>
+        <Typography variant="body1" sx={{ mt: 5, mb: 5 }}>
+        Référencez-vous sur Stacker.io et faîtes découvrir vos atouts auprès de nos milliers de développeurs.
+        </Typography>
+      </div>
+    </div>
+  </div>
   );
 };
 
--- src/Pages/SearchCompany/index.jsx
+++ src/Pages/SearchCompany/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './searchCompany.scss';
 import { FilterContext } from '../../Context/FilterContext';
 import SavedSearch from '../../Components/SavedSearch';
