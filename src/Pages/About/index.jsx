a941a5b13804c1cdfaa6f142022192725b3e3260
VeraTuran <vmeturan@gmail.com>
2021-09-24 19:49:10 +0200

    responsive-ish

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
 import lottie from "lottie-web";
 //styles
 import "./about.scss";
@@ -8,13 +9,12 @@ import "aos/dist/aos.css";
 import Typography from "@mui/material/Typography";
 //Typewriter
 import Typewriter from "typewriter-effect";
-import { Paper } from "@material-ui/core";
-import UIButton from "../../Components/UIButton";
+import { Paper, Button } from "@material-ui/core";
 
 const About = () => {
   const container = useRef(null);
 
-  useEffect (() => {
+  useEffect(() => {
     Aos.init({});
   }, []);
 
@@ -29,67 +29,72 @@ const About = () => {
   }, []);
 
   return (
-  <div className="container__home">
-    <div className="container__home--left">
-      <div className="content--text">
-        <Typography variant="h1" component="h1"> 
-          Stacker c'est
-          <Typewriter
-            onInit={(typewriter) => {
-              typewriter.typeString("répérer")
-                .pauseFor(2500)
-                .start()
-                .pauseFor(200)
-                .deleteChars(7)
-                .typeString("découvrir")
-                .pauseFor(2500)
-                .start()
-                .pauseFor(200)
-                .deleteChars(9)
-                .typeString("trouver");
-            }}
-          />
-        </Typography>
-        <Typography variant="body1" sx={{ mt: 5, mb: 5 }}>
-          Stacker.io offre de naviguer des entreprises par leur Stack pour les devlopeurs freelance ou en recherche d'emploi. <br />
-          En tant que devs en fin de formation, nous voulions rendre plus souple et transparent le processus pour chacun de trouver sa niche selon son expertise.
-        </Typography>
+    <div className="container__about">
+      <div className="container__about--left">
+        <div className="content--text--about">
+          <Typography variant="h1" component="h1">
+            Stacker c'est
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("répérer")
+                  .pauseFor(2500)
+                  .start()
+                  .pauseFor(200)
+                  .deleteChars(7)
+                  .typeString("découvrir")
+                  .pauseFor(2500)
+                  .start()
+                  .pauseFor(200)
+                  .deleteChars(9)
+                  .typeString("trouver");
+              }}
+            />
+          </Typography>
+          <Typography variant="body1" sx={{ mt: 5, mb: 5 }}>
+            Stacker.io offre de naviguer des entreprises par leur Stack pour les
+            devlopeurs freelance ou en recherche d'emploi. <br />
+            En tant que devs en fin de formation, nous voulions rendre plus
+            souple et transparent le processus pour chacun de trouver sa niche
+            selon son expertise.
+          </Typography>
+          <Typography variant="h2" component="h2">
+            Pour Devs
+          </Typography>
+          <Typography variant="body1" sx={{ mt: 5, mb: 5 }}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+            blanditiis tenetur unde suscipit, quam beatae rerum inventore
+            consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+            fugiat deleniti? Eum quasi quidem quibusdam.
+          </Typography>
+          <Typography variant="h2" component="h2">
+            Pour Entreprise
+          </Typography>
+          <Typography variant="body1" sx={{ mt: 5, mb: 5 }}>
+            Référencez-vous sur Stacker.io et faîtes découvrir vos atouts auprès
+            de nos milliers de développeurs.
+          </Typography>
+          <div className="CTA--about">
+            <Button
+              component={Link}
+              to="/search/company"
+              size="large"
+              variant="outlined"
+              style={{
+                color: "white",
+                borderColor: "white",
+              }}
+            >
+              découvrir stacker.io
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="container__aboutsvg--right">
+        <Paper elevation={10} className="mask--color--about"></Paper>
+        <div className="content--svg--about" ref={container}></div>
       </div>
     </div>
-    <div className="container__home--right">
-      <Paper elevation={10} className="mask--color"></Paper>
-      <div className="content--svg" ref={container}></div>
-    </div>
-
-    <div className="container__about--right" >
-      <div className="content--text">
-      <Typography variant="h2" component="h2">Pour Devs</Typography>
-        <Typography variant="body1" sx={{ mt: 5, mb: 5 }}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-          blanditiis tenetur unde suscipit, quam beatae rerum inventore
-          consectetur, neque doloribus, cupiditate numquam dignissimos laborum
-          fugiat deleniti? Eum quasi quidem quibusdam.
-        </Typography>
-      </div>
-    </div>
-
-    <div className="container__about--right">
-      <div className="content--text">
-      <Typography variant="h2" component="h2">Pour Entreprise</Typography>
-        <Typography variant="body1" sx={{ mt: 5, mb: 5 }}>
-        Référencez-vous sur Stacker.io et faîtes découvrir vos atouts auprès de nos milliers de développeurs.
-        </Typography>
-      </div>
-      <div className="container__home--left">
-      <UIButton
-        color="inherit"
-        size="large"
-        variant="outlined"
-        content="découvrir stacker.io"
-      />
-    </div>
-    </div>
-  </div>
   );
 };
 
